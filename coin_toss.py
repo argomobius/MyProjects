18eab7fa82659811f624f6e67514a5c59427ddb3
unknown <255978@pn1172037.lanl.gov>
2014-04-25 17:15:01 -0600

    Added coin_toss module

---
+++ coin_toss.py
@@ -0,0 +1,68 @@
+import socket
+import sys
+import time
+
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+except socket.error, msg:
+	print "Failed to create socket. Error code: " + str(msg[0]) + "Error message: " + str(msg[1])
+	sys.exit();
+
+print 'Socket Created'
+
+host = '' #All available interfaces
+port = 8908 #arbitrary port
+
+try: 
+	s.bind((host, port))
+
+except socket.error, msg:
+	print "Failed to bind socket. Error code: " + str(msg[0]) + " Error message: " + str(msg[1])
+	sys.exit();
+
+print 'Socket bind complete'	
+
+s.listen(10)
+
+print 'Socket now listening'
+
+
+def clientthread(conn):
+
+	conn.send('Welcome to the echo server. Type something and hit enter \n')
+
+	data = ''
+
+	while True:
+
+		data += conn.recv(1096)
+		
+		conn.send(data)	
+
+	conn.close()
+
+
+while 1:	
+
+	conn, addr = s.accept()
+
+	print 'Connected with ' + addr[0] + ':' + str(addr[1])  
+
+	clientthread(conn)
+
+s.close()
+
+
+#def coin():
+	result = random.randrange(1,3)
+	if result == 1:
+		print "Heads!"
+	else:
+		print "Tails!"
+
+
+
+
+
+
